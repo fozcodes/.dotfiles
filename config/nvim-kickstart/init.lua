72f4f22ebcb14a98fe6445c3c5124b6423531baa
fozcodes <ian.fosbery@reaktor.com>
2024-04-09 21:47:01 -0400

    Change kitty background and fix tabs/spaces in nvim

--- config/nvim-kickstart/init.lua
+++ config/nvim-kickstart/init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -153,6 +153,12 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- tabs can go to hell
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
