66640c07dae0f2a52048583ff31440c2401e9f04
fozcodes <ian.fosbery@reaktor.com>
2024-04-09 21:37:04 -0400

    Add golden ratio and nightfox

--- config/nvim-kickstart/init.lua
+++ config/nvim-kickstart/init.lua
@@ -250,6 +250,7 @@ require("lazy").setup({
 	{ "numToStr/Comment.nvim", opts = {} },
 
 	"christoomey/vim-tmux-navigator",
+	"roman/golden-ratio",
 
 	-- Here is a more advanced example where we pass configuration
 	-- options to `gitsigns.nvim`. This is equivalent to the following Lua:
@@ -774,13 +775,28 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
+		"EdenEast/nightfox.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
+		opts = {
+			fox = "dayfox",
+			transparent = true,
+			italic_keywords = true,
+			terminal_colors = true,
+			styles = {
+				functions = "italic,bold", -- styles can be a comma separated list
+			},
+			inverse = {
+				match_paren = true, -- inverse the highlighting of match_parens
+			},
+			colors = {
+				red = "#FFCCCB", -- Override the red color for MAX POWER
+			},
+		},
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("nordfox")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
