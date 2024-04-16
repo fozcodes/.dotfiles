9fba27de6144d5d7905fd03d27a8fa5c06f206f2
fozcodes <ian.fosbery@reaktor.com>
2024-04-16 15:13:51 -0400

    Add rainbow csv

--- config/nvim-kickstart/init.lua
+++ config/nvim-kickstart/init.lua
@@ -269,7 +269,7 @@ require("lazy").setup({
   "roman/golden-ratio",
   "mhinz/vim-startify",
   "easymotion/vim-easymotion",
-
+  "mechatroner/rainbow_csv",
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
