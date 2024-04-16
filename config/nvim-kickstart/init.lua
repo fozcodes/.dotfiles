2224221b603e254238af4e6551a8648d9ee05ba4
fozcodes <ian.fosbery@reaktor.com>
2024-04-16 14:39:06 -0400

    Add header to init and lsp log finder

--- config/nvim-kickstart/init.lua
+++ config/nvim-kickstart/init.lua
@@ -1,94 +1,44 @@
 --[[
+ ______   ______     ______  ______   ______   ______   ______   ______
+/_____/\ /_____/\   /_____/\/_____/\ /_____/\ /_____/\ /_____/\ /_____/\
+\::::_\/_\:::_ \ \  \:::__\/\:::__\/ \:::_ \ \\:::_ \ \\::::_\/_\::::_\/_
+ \:\/___/\\:\ \ \ \    /: /  \:\ \  __\:\ \ \ \\:\ \ \ \\:\/___/\\:\/___/\
+  \:::._\/ \:\ \ \ \  /::/___ \:\ \/_/\\:\ \ \ \\:\ \ \ \\::___\/_\_::._\:\
+   \:\ \    \:\_\ \ \/_:/____/\\:\_\ \ \\:\_\ \ \\:\/.:| |\:\____/\ /____\:\
+    \_\/     \_____\/\_______\/ \_____\/ \_____\/ \____/_/ \_____\/ \_____\/
 
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
 
-What is Kickstart?
 
-  Kickstart.nvim is *not* a distribution.
+ __  __  _____        ____    ____    ______  __      __      __    __
+/\ \/\ \/\  __`\     /\  _`\ /\  _`\ /\  _  \/\ \    /\ \    /\ \  /\ \
+\ \ `\\ \ \ \/\ \    \ \ \L\ \ \ \L\_\ \ \L\ \ \ \   \ \ \   \ `\`\\/'/
+ \ \ , ` \ \ \ \ \    \ \ ,  /\ \  _\L\ \  __ \ \ \  _\ \ \  _`\ `\ /'
+  \ \ \`\ \ \ \_\ \    \ \ \\ \\ \ \L\ \ \ \/\ \ \ \L\ \ \ \L\ \`\ \ \ __
+   \ \_\ \_\ \_____\    \ \_\ \_\ \____/\ \_\ \_\ \____/\ \____/  \ \_/\ \
+    \/_/\/_/\/_____/     \/_/\/ /\/___/  \/_/\/_/\/___/  \/___/    \/_\ \/
+                                                                       \/
+ __  __  ____          ____    _____   ____    ____    __
+/\ \/\ \/\  _`\       /\  _`\ /\  __`\/\  _`\ /\  _`\ /\ \
+\ \ \_\ \ \ \L\_\     \ \ \/\ \ \ \/\ \ \ \L\_\ \,\L\_\ \ \
+ \ \  _  \ \  _\L      \ \ \ \ \ \ \ \ \ \  _\L\/_\__ \\ \ \
+  \ \ \ \ \ \ \L\ \     \ \ \_\ \ \ \_\ \ \ \L\ \/\ \L\ \ \_\
+   \ \_\ \_\ \____/      \ \____/\ \_____\ \____/\ `\____\/\_\
+    \/_/\/_/\/___/        \/___/  \/_____/\/___/  \/_____/\/_/
+]]
 
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
+-- FIND MY LSP LOG {{{
+-- Uncomment the prints and start nvim to find the goddam LSP logfile again. Cause it ain't where you think!
+-- vim.g.lua_lsp_log_file = os.getenv "HOME" .. "/.cache/nvim/lsp.log"
+-- print "THIS IS THE LOG PATH"
+-- print(vim.lsp.get_log_path())
+-- vim.lsp.set_log_level "debug"
+-- }}}
 
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
+-- Leader key {{{
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
+-- }}}
 
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
