f26f6d13c420382cd669c1923ea8afb9fdb74348
fozcodes <ian.fosbery@paramount.com>
2025-08-03 21:38:39 -0400

    Add aws function for loading keys

--- config/fish/conf.d/aws.fish
+++ config/fish/conf.d/aws.fish
@@ -13,6 +13,43 @@ function assume-aws-role
   set -Ux AWS_SESSION_TOKEN $aws_session_token
 end
 
+function add-keys-from-aws-profile
+    set profile $argv[1]
+
+    if test -z "$profile"
+        echo "Usage: add-keys-from-aws-profile <profile-name>"
+        return 1
+    end
+
+    set aws_key_id (awk -v p="[$profile]" '
+        $0 == p {found=1}
+        found && $1 == "aws_access_key_id" {print $3; exit}
+    ' ~/.aws/credentials)
+
+    set aws_secret_key (awk -v p="[$profile]" '
+        $0 == p {found=1}
+        found && $1 == "aws_secret_access_key" {print $3; exit}
+    ' ~/.aws/credentials)
+
+    set aws_session_token (awk -v p="[$profile]" '
+        $0 == p {found=1}
+        found && $1 == "aws_session_token" {print $3; exit}
+    ' ~/.aws/credentials)
+
+    if test -z "$aws_key_id" -o -z "$aws_secret_key"
+        echo "Missing credentials for profile: $profile"
+        return 1
+    end
+
+  echo $aws_key_id
+  echo $aws_secret_key
+  echo $aws_session_token
+
+  set -Ux AWS_ACCESS_KEY_ID $aws_key_id
+  set -Ux AWS_SECRET_ACCESS_KEY $aws_secret_key
+  set -Ux AWS_SESSION_TOKEN $aws_session_token
+end
+
 function unset-aws-creds
   set -e AWS_ACCESS_KEY_ID
   set -e AWS_SECRET_ACCESS_KEY
