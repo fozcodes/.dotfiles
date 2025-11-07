fd37e2927357afb78218beffeeaed90897fcf0c7
fozcodes <ian.fosbery@paramount.com>
2025-11-07 12:56:48 -0500

    Put back copilot and telescope versions

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -292,6 +292,7 @@ require("lazy").setup({
   -- "mhinz/vim-startify",
   "easymotion/vim-easymotion",
   "mechatroner/rainbow_csv",
+  "github/copilot.vim",
   {
     "rubiin/fortune.nvim",
     config = function()
@@ -351,8 +352,7 @@ require("lazy").setup({
   { -- Fuzzy Finder (files, lsp, etc)
     "nvim-telescope/telescope.nvim",
     event = "VimEnter",
-    -- branch = "0.1.x",
-    commit = "b4da76be54691e854d3e0e02c36b0245f945c2c7",
+    branch = "0.1.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -461,7 +461,21 @@ require("lazy").setup({
       vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
       vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
       vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+      vim.keymap.set("n", "<leader>sw", function()
+        builtin.grep_string {
+          additional_args = function()
+            return { "--no-ignore" }
+          end,
+        }
+      end, { desc = "[S]earch current [W]ord (no ignore)" })
       vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader>sG", function()
+        builtin.live_grep {
+          additional_args = function()
+            return { "--no-ignore" }
+          end,
+        }
+      end, { desc = "[S]earch by [g]rep (no ignore)" })
       vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
       vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
       vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
